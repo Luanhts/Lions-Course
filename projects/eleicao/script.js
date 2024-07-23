28fb790101bf5168c993d33cbbae95ed31b78299
Luanhts <luanh3nrique08@gmail.com>
2024-07-23 19:35:23 -0300

    Mais uma atividade

---
+++ projects/eleicao/script.js
@@ -0,0 +1,16 @@
+const prompt = require("prompt-sync")();
+
+var users = []
+
+console.log("Bem vindo a sistema de votos: ")
+
+const menu = () => {
+    console.log("Sitema de votos")
+    console.log(`
+                1. Cadastrar Usuário
+                2. Sair`)
+}
+
+
+
+
